881399dc9d588d1c1c3812e745dd3831c3455583
Andre Fontenele <andreft316@gmail.com>
2025-04-10 21:12:58 -0400

    fixed typo and updated model associations for student and course tables regarding each other

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,7 @@ class Course < ApplicationRecord
 
   has_many :teacher_assignments
   has_many :account_teachers, through: :teacher_assignments
+
+  has_many :course_registrations
+  has_many :students, through: :course_registrations
 end
