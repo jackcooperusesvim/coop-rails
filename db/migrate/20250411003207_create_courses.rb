67697206cfaf1d5371fdcc5bd7f312cfc8c55f41
Andre Fontenele <andreft316@gmail.com>
2025-04-10 20:43:42 -0400

    Removed course-teacher association, and renaming "period" to "semester".

--- db/migrate/20250411003207_create_courses.rb
+++ db/migrate/20250411003207_create_courses.rb
@@ -3,8 +3,7 @@ class CreateCourses < ActiveRecord::Migration[8.0]
     create_table :courses do |t|
       t.string :name
       t.text :desc
-      t.references :teacher, null: false, foreign_key: true
-      t.references :period, null: false, foreign_key: true
+      t.references :semester, null: false, foreign_key: true
 
       t.timestamps
     end
