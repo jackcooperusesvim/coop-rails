941725e8c91da251e4cd05406a57c3f1a70badbf
Andre Fontenele <andreft316@gmail.com>
2025-04-10 20:26:46 -0400

    generated basic teacher model. May change in the future, as I learn more about the problem space.

---
+++ db/migrate/20250411002609_create_teachers.rb
@@ -0,0 +1,13 @@
+class CreateTeachers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.integer :phone1
+      t.integer :phone2
+      t.string :sec_email
+      t.string :note
+
+      t.timestamps
+    end
+  end
+end
